fc9f27e8955a5488005a7fe7b1f83772d3202253
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-26 22:37:58 +0300

    Bizo fan

---
+++ col.py
@@ -0,0 +1,12 @@
+#Bizo is a fan with a thermometer fixed.its in AnitaB class,write a code of the use of the fan in class
+from tokenize import Double
+
+
+a=Double((input("Enter room temperature :")))
+# for a in range(30 or 60):
+    # print("Turn fun on")
+if(a>30):
+        print("Turn fan on")
+elif (a<30):
+        print("Class is good temperature")
+    
