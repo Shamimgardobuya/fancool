15d006e741e552ba4a665262fc2de5a7977adfdd
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-01 20:59:22 +0300

    fan on

--- col.py
+++ col.py
@@ -1,12 +1,12 @@
 #Bizo is a fan with a thermometer fixed.its in AnitaB class,write a code of the use of the fan in class
-from tokenize import Double
+# from tokenize import Double
+#got an error that stated my str object is not callable meaning that there is a problem the way i callde my variable.
 
-
-a=Double((input("Enter room temperature :")))
+a=int(input("Enter room temperature "))
 # for a in range(30 or 60):
     # print("Turn fun on")
 if(a>30):
         print("Turn fan on")
 elif (a<30):
-        print("Class is good temperature")
+        print("Class is cool temperature")
     
